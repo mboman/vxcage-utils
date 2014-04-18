97ec6139886c3ecf90648b27706b20ef3942aa9f
Michael Boman <michael@michaelboman.org>
2014-04-18 15:35:42 +0200

    Unified utils.py between vxcage, vxcage-jobs and vxcage-utils. It is now the same file. This also mean that the configuration file has to have the same name between the different projects.

--- file2mongodb.py
+++ file2mongodb.py
@@ -1,129 +1,108 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+import StringIO
+import hashlib
+import logging
 import os
 import sys
 import zipfile
-import logging
-from pymongo import MongoClient
-import gridfs
-import StringIO
-import hashlib
-from utils import Config
 
 try:
     import magic
 except ImportError:
     pass
 
-JOBNAME = "FILE2MONGODB"
-FILE_CHUNK_SIZE = 16 * 1024
+from pymongo import MongoClient
+import gridfs
 
-def get_chunks(data):
-    """Read file contents in chunks (generator)."""
-    fd = StringIO.StringIO(data)
-    while True:
-        chunk = fd.read(FILE_CHUNK_SIZE)
-        if not chunk: break
-        yield chunk
-    fd.close()
+from utils import Config, get_type, put_file, get_chunks
+
+
+JOBNAME = 'FILE2MONGODB'
 
 # create logger
+
 logger = logging.getLogger(JOBNAME)
 logger.setLevel(logging.DEBUG)
 
 # create console handler with a higher log level
+
 logch = logging.StreamHandler()
 logch.setLevel(logging.DEBUG)
 
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+formatter = \
+    logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+                      )
 logch.setFormatter(formatter)
 
 # add the handlers to the logger
+
 logger.addHandler(logch)
 
 # Connect to MongoDB
-client = MongoClient(host=Config().job.dbhost, port=Config().job.dbport)
+
+client = MongoClient(host=Config().database.dbhost,
+                     port=Config().database.dbport)
 db = client.vxcage
 fs = gridfs.GridFS(db)
 
-logging.basicConfig(
-    format = "%(levelname) -10s %(asctime)s %(message)s",
-    level = logging.DEBUG
-)
+logging.basicConfig(format='%(levelname) -10s %(asctime)s %(message)s',
+                    level=logging.DEBUG)
 
-def get_type(file_data):
-    try:
-        ms = magic.open(magic.MAGIC_NONE)
-        ms.load()
-        file_type = ms.buffer(file_data)
-        logging.debug("Got magic through method #1")
-    except:
-        try:
-            file_type = magic.from_buffer(file_data)
-            logging.debug("Got magic through method #2")
-        except:
-            try:
-                import subprocess
-                file_path = tempfile.NamedTemporaryFile(mode='w+b')
-                file_path.write(file_data)
-                file_path.flush()
-                file_process = subprocess.Popen(['file', '-b', file_path], stdout = subprocess.PIPE)
-                file_type = file_process.stdout.read().strip()
-                file_path.close()
-                logging.debug("Got magic through method #3")
-            except:
-                return None
+numOfFiles = len(sys.argv[1:])
 
-    return file_type
-
-numOfFiles=len(sys.argv[1:])
-
-for index,sampleEntry in enumerate(sys.argv[1:]):
-    logging.info("[%s] Got entry (%s/%s)" % (sampleEntry, index+1, numOfFiles))
+for (index, sampleEntry) in enumerate(sys.argv[1:]):
+    logging.info('[%s] Got entry (%s/%s)' % (sampleEntry, index + 1,
+                 numOfFiles))
     filename = os.path.basename(sampleEntry)
     if not os.path.isfile(sampleEntry):
-        logging.debug("Not a file. Skipping")
+        logging.debug('Not a file. Skipping')
         continue
     sample = open(sampleEntry, 'rb')
 
     sampleData = sample.read()
 
-    logging.debug("[%s] Generating hashes" % (sampleEntry))
+    logging.debug('[%s] Generating hashes' % sampleEntry)
     md5 = hashlib.md5(sampleData).hexdigest()
     sha1 = hashlib.sha1(sampleData).hexdigest()
     sha256 = hashlib.sha256(sampleData).hexdigest()
-    sha512 =  hashlib.sha512(sampleData).hexdigest()
+    sha512 = hashlib.sha512(sampleData).hexdigest()
+    filetype = get_filetype(sampleData)
 
-    logging.debug("[%s] Quering database for already existing file (hash=%s)" % (sampleEntry, sha256))
-    existing = db.fs.files.find_one({"sha256": sha256})
+    logging.debug('[%s] Quering database for already existing file (hash=%s)'
+                   % (sampleEntry, sha256))
+    existing = db.fs.files.find_one({'sha256': sha256})
 
-    upload_sample = True    
+    upload_sample = True
     if existing:
-        logging.info("[%s] Sample already exists" % (sampleEntry))
-        logging.info("[%s] Verifying contents" % (sampleEntry))
+        logging.info('[%s] Sample already exists' % sampleEntry)
+        logging.info('[%s] Verifying contents' % sampleEntry)
         if not md5 == existing['md5']:
-            logging.warning("[%s] Checksum not matching" % (sampleEntry))
+            logging.warning('[%s] Checksum not matching' % sampleEntry)
             upload_sample = True
         else:
-            logging.info("[%s] Checksum matching" % (sampleEntry))
+            logging.info('[%s] Checksum matching' % sampleEntry)
             upload_sample = False
     else:
         upload_sample = True
 
     if upload_sample:
-        logging.debug("[%s] Uploading sample" % (sampleEntry))
-        new = fs.new_file(filename=filename, sha1=sha1, sha256=sha256, sha512=sha512)
+        logging.debug('[%s] Uploading sample' % sampleEntry)
+        new = fs.new_file(filename=filename, sha1=sha1, sha256=sha256,
+                          sha512=sha512, filetype=filetype)
         for chunk in get_chunks(sampleData):
-            logging.debug("[%s] writing chunk" % (sampleEntry))
+            logging.debug('[%s] writing chunk' % sampleEntry)
             new.write(chunk)
         new.close()
-        logging.info("[%s] Uploaded sample" % (sampleEntry))
-    logging.debug("[%s] Reclaiming memory" % (sampleEntry))
+        logging.info('[%s] Uploaded sample' % sampleEntry)
+    logging.debug('[%s] Reclaiming memory' % sampleEntry)
     sample.close()
 
     del sample
     del sampleData
-    logging.debug("[%s] Deleting file" % (sampleEntry))
+    logging.debug('[%s] Deleting file' % sampleEntry)
     os.remove(sampleEntry)
 
--- zip2mongodb.py
+++ zip2mongodb.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -8,99 +9,150 @@ from pymongo import MongoClient
 import gridfs
 import StringIO
 import hashlib
+from utils import Config
 
-JOBNAME="ZIP2MONGODB"
+try:
+    import magic
+except ImportError:
+    pass
+
+JOBNAME = 'ZIP2MONGODB'
 FILE_CHUNK_SIZE = 16 * 1024
+
 
 def get_chunks(data):
     """Read file contents in chunks (generator)."""
+
     fd = StringIO.StringIO(data)
     while True:
         chunk = fd.read(FILE_CHUNK_SIZE)
-        if not chunk: break
+        if not chunk:
+            break
         yield chunk
     fd.close()
 
+
 # create logger
+
 logger = logging.getLogger(JOBNAME)
 logger.setLevel(logging.DEBUG)
 
-# create file handler which logs even debug messages
-#logfh = logging.FileHandler(JOBNAME + '.log')
-#logfh.setLevel(logging.DEBUG)
-
 # create console handler with a higher log level
+
 logch = logging.StreamHandler()
 logch.setLevel(logging.DEBUG)
 
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#logfh.setFormatter(formatter)
+
+formatter = \
+    logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+                      )
 logch.setFormatter(formatter)
 
 # add the handlers to the logger
-#logger.addHandler(logfh)
+
 logger.addHandler(logch)
 
 # Connect to MongoDB
-client = MongoClient('mart-mongodb01')
+
+client = MongoClient(host=Config().database.dbhost,
+                     port=Config().database.dbport)
 db = client.vxcage
 fs = gridfs.GridFS(db)
 
-logging.basicConfig(
-    format = "%(levelname) -10s %(asctime)s %(message)s",
-    level = logging.DEBUG
-)
+logging.basicConfig(format='%(levelname) -10s %(asctime)s %(message)s',
+                    level=logging.DEBUG)
 
 
-logging.info("[%s] Opening" % (sys.argv[1]))
+def get_type(file_data):
+    try:
+        ms = magic.open(magic.MAGIC_NONE)
+        ms.load()
+        file_type = ms.buffer(file_data)
+        logging.debug('Got magic through method #1')
+    except:
+        try:
+            file_type = magic.from_buffer(file_data)
+            logging.debug('Got magic through method #2')
+        except:
+            try:
+                import subprocess
+                file_path = tempfile.NamedTemporaryFile(mode='w+b')
+                file_path.write(file_data)
+                file_path.flush()
+                file_process = subprocess.Popen(['file', '-b',
+                        file_path], stdout=subprocess.PIPE)
+                file_type = file_process.stdout.read().strip()
+                file_path.close()
+                logging.debug('Got magic through method #3')
+            except:
+                return None
+
+    return file_type
+
+
+logging.info('[%s] Opening' % sys.argv[1])
 archive = zipfile.ZipFile(sys.argv[1])
 zipname = os.path.splitext(os.path.basename(sys.argv[1]))[0]
-archive.setpassword("infected")
+archive.setpassword('infected')
 
 numOfFiles = len(archive.namelist())
 
-for index,sampleEntry in enumerate(archive.namelist()):
-    logging.info("[%s] [%s] Got entry (%s/%s)" % (zipname, sampleEntry, index+1, numOfFiles))
+for (index, sampleEntry) in enumerate(archive.namelist()):
+    logging.info('[%s] [%s] Got entry (%s/%s)' % (zipname, sampleEntry,
+                 index + 1, numOfFiles))
     sample = archive.open(sampleEntry, 'r')
 
     sampleData = sample.read()
 
-    logging.debug("[%s] [%s] Generating hashes" % (zipname, sampleEntry))
+    logging.debug('[%s] [%s] Generating hashes' % (zipname,
+                  sampleEntry))
     md5 = hashlib.md5(sampleData).hexdigest()
     sha1 = hashlib.sha1(sampleData).hexdigest()
     sha256 = hashlib.sha256(sampleData).hexdigest()
-    sha512 =  hashlib.sha512(sampleData).hexdigest()
+    sha512 = hashlib.sha512(sampleData).hexdigest()
+    filetype = get_filetype(sampleData)
 
-    logging.debug("[%s] [%s] Quering database for already existing file (hash=%s)" % (zipname, sampleEntry, sha256))
-    existing = db.fs.files.find_one({"sha256": sha256})
+    logging.debug('[%s] [%s] Quering database for already existing file (hash=%s)'
+                   % (zipname, sampleEntry, sha256))
+    existing = db.fs.files.find_one({'sha256': sha256})
 
-    upload_sample = True    
+    upload_sample = True
     if existing:
-        logging.info("[%s] [%s] Sample already exists" % (zipname, sampleEntry))
-        logging.info("[%s] [%s] Verifying contents" % (zipname, sampleEntry))
+        logging.info('[%s] [%s] Sample already exists' % (zipname,
+                     sampleEntry))
+        logging.info('[%s] [%s] Verifying contents' % (zipname,
+                     sampleEntry))
         if not md5 == existing['md5']:
-            logging.warning("[%s] [%s] Checksum not matching" % (zipname, sampleEntry))
+            logging.warning('[%s] [%s] Checksum not matching'
+                            % (zipname, sampleEntry))
             upload_sample = True
         else:
-            logging.info("[%s] [%s] Checksum matching" % (zipname, sampleEntry))
+            logging.info('[%s] [%s] Checksum matching' % (zipname,
+                         sampleEntry))
             upload_sample = False
     else:
         upload_sample = True
 
     if upload_sample:
-        logging.debug("[%s] [%s] Uploading sample" % (zipname, sampleEntry))
-        new = fs.new_file(filename=sampleEntry, sha1=sha1, sha256=sha256, sha512=sha512)
+        logging.debug('[%s] [%s] Uploading sample' % (zipname,
+                      sampleEntry))
+        new = fs.new_file(filename=sampleEntry, sha1=sha1,
+                          sha256=sha256, sha512=sha512,
+                          filetype=filetype)
         for chunk in get_chunks(sampleData):
-            logging.debug("[%s] [%s] writing chunk" % (zipname, sampleEntry))
+            logging.debug('[%s] [%s] writing chunk' % (zipname,
+                          sampleEntry))
             new.write(chunk)
         new.close()
-        logging.info("[%s] [%s] Uploaded sample" % (zipname, sampleEntry))
-    logging.debug("[%s] [%s] Reclaiming memory" % (zipname, sampleEntry))
+        logging.info('[%s] [%s] Uploaded sample' % (zipname,
+                     sampleEntry))
+    logging.debug('[%s] [%s] Reclaiming memory' % (zipname,
+                  sampleEntry))
     sample.close()
 
     del sample
     del sampleData
 
-logging.info("[%s] Closing" % (zipname))
+logging.info('[%s] Closing' % zipname)
 archive.close()
